02ed5b6389ad3ef7d41cfb7782da9a7bb84eb99d
Eduardo Barros <eduardo.barros@executiva.com.br>
2021-10-21 23:49:45 -0300

    Update

--- api/controllers/puppeteerController.js
+++ api/controllers/puppeteerController.js
@@ -14,81 +14,86 @@ exports.get = async (req, res, next) => {
 }
 
 exports.post = async (req, res, next) => {
-    console.log('iniciando browser');
-    const {browser, page} = await startBrowser();
+    try {
+        console.log('iniciando browser');
+        const {browser, page} = await startBrowser();
 
-    await page.goto(config.get('salesforce.url'));
+        await page.goto(config.get('salesforce.url'));
 
-    console.log('realizando login');
-    await page.waitForSelector(config.get('selectors.username'), {timeout: 10000});
-    await page.click(config.get('selectors.username'));
-    await page.keyboard.type(config.get('credentials.username'));
+        console.log('realizando login');
+        await page.waitForSelector(config.get('selectors.username'), {timeout: 10000});
+        await page.click(config.get('selectors.username'));
+        await page.keyboard.type(config.get('credentials.username'));
 
-    await page.waitForSelector(config.get('selectors.password'), {timeout: 10000});
-    await page.click(config.get('selectors.password'));
-    await page.keyboard.type(config.get('credentials.password'));
+        await page.waitForSelector(config.get('selectors.password'), {timeout: 10000});
+        await page.click(config.get('selectors.password'));
+        await page.keyboard.type(config.get('credentials.password'));
 
-    console.log('acessando pagina home');
-    await page.click(config.get('selectors.login'));
-    await page.waitForNavigation();
+        console.log('acessando pagina home');
+        await page.click(config.get('selectors.login'));
+        await page.waitForNavigation();
 
-    console.log('acessando validacao em lote');
-    await page.goto(config.get('salesforce.url_valid_lot'));
+        console.log('acessando validacao em lote');
+        await page.goto(config.get('salesforce.url_valid_lot'));
 
-    console.log('iniciando consulta de cnpj');
-    await page.waitForSelector(config.get('selectors.search'), {timeout: 10000});
-    await page.click(config.get('selectors.search'));
+        console.log('iniciando consulta de cnpj');
+        await page.waitForSelector(config.get('selectors.search'), {timeout: 10000});
+        await page.click(config.get('selectors.search'));
 
-    const docs = req.body;
+        const docs = req.body;
 
-    for (const key of Object.keys(docs)) {
-        await page.keyboard.type(key);
-    }
-
-    await Promise.all([
-        await page.click(config.get('selectors.search_button'))
-    ]);
-
-    console.log('aguardando resultados')
-    await page.waitForSelector(config.get('selectors.table'), {timeout: 10000});
-
-    console.log('salvando resultados')
-    const bodyHTML = await page.evaluate(() => document.querySelector('*').outerHTML);
-
-    fs.writeFile(path.join(__dirname + '/destination.html'), bodyHTML, function (err) {
-        if (err) {
-            return console.log(err);
+        for (const key of Object.keys(docs)) {
+            await page.keyboard.type(key);
         }
-    });
 
-    await page.goto(`http://localhost:` + config.get('server.port') + `/result`);
+        await Promise.all([
+            await page.click(config.get('selectors.search_button'))
+        ]);
 
-    const results = await page.evaluate(() => {
-        const array = [];
-        const tables = document.querySelectorAll('table');
-        for (a = 0; a < tables.length; a++) {
-            for (b = 0; b < tables[a].children[1].childElementCount; b++) {
-                let document = tables[a].children[1].children[b].children[0].children[0].innerText;
-                let message = tables[a].children[1].children[b].children[1].children[0].innerText;
-                let products = tables[a].children[1].children[b].children[2].children[0].innerText;
-                let event = {document, message, products};
-                array.push(event);
+        console.log('aguardando resultados')
+        await page.waitForSelector(config.get('selectors.table'), {timeout: 10000});
+
+        console.log('salvando resultados')
+        const bodyHTML = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+        fs.writeFile(path.join(__dirname + '/destination.html'), bodyHTML, function (err) {
+            if (err) {
+                return console.log(err);
             }
-        }
-        return array;
-    },);
+        });
 
-    for (a = 0; a < results.length; a++) {
+        await page.goto(`http://localhost:` + config.get('server.port') + `/result`);
+
+        const results = await page.evaluate(() => {
+            const array = [];
+            const tables = document.querySelectorAll('table');
+            for (a = 0; a < tables.length; a++) {
+                for (b = 0; b < tables[a].children[1].childElementCount; b++) {
+                    let document = tables[a].children[1].children[b].children[0].children[0].innerText;
+                    let message = tables[a].children[1].children[b].children[1].children[0].innerText;
+                    let products = tables[a].children[1].children[b].children[2].children[0].innerText;
+                    let event = {document, message, products};
+                    array.push(event);
+                }
+            }
+            return array;
+        },);
+
+        for (a = 0; a < results.length; a++) {
             results[a].id = docs[results[a].document].id
+        }
+
+        fs.unlink(path.join(__dirname + '/destination.html'), function (err) {
+            if (err) throw err;
+            console.log('deletando resultados');
+        });
+
+        console.log('fechando browser');
+        await browser.close();
+
+        res.send(results);
     }
-
-    fs.unlink(path.join(__dirname + '/destination.html'), function (err) {
-        if (err) throw err;
-        console.log('deletando resultados');
-    });
-
-    console.log('fechando browser');
-    await browser.close();
-
-    res.send(results);
+    catch (e) {
+        return false;
+    }
 };
