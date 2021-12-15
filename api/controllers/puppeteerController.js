f3e49223210cbdd7deeb05cd8a66b3ce1a944d23
Eduardo Barros <eduardo.barros@executiva.com.br>
2021-12-15 13:17:59 -0300

    Update

--- api/controllers/puppeteerController.js
+++ api/controllers/puppeteerController.js
@@ -51,7 +51,7 @@ exports.post = async (req, res, next) => {
         ]);
 
         console.log('aguardando resultados')
-        await page.waitForSelector(config.get('selectors.table'), {timeout: 10000});
+        await page.waitForSelector(config.get('selectors.table'), {timeout: 20000});
 
         console.log('salvando html')
         let bodyHTML = await page.evaluate(() => document.querySelector('*').innerHTML);
